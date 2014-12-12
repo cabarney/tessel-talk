dba9b0c738918a6f590008f4e5d8b81965b3bc3e
Adam Barney <adam@adambarney.com>
2014-12-12 13:39:59 -0600

    Audio

---
+++ demos/audio/audio.js
@@ -0,0 +1,10 @@
+var tessel = require('tessel');
+var fs = require('fs');
+var audio = require('audio-vs1053b').use(tessel.port['A']);
+
+audio.on('ready', function () {
+  audio.setOutput('headphones', function(err) {
+    var audioFile = fs.readFileSync('audio.mp3');
+    audio.play(audioFile);
+  });
+});
